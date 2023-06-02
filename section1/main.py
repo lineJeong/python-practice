307ae240188349e266b5d049bf6043d9b6ba5f3d
lineJeong <line.jeong32@gmail.com>
2023-06-02 19:53:11 +0900

    section1 - 15. 1일차 프로젝트 : 밴드명 생성기

--- section1/main.py
+++ section1/main.py
@@ -2,12 +2,17 @@
 
 # print("Hello " + input("what is your name?") + "!")
 
-name = "Jack"
-print(name)
+# name = "Jack"
+# print(name)
 
-name = "Eden"
-print(name)
+# name = "Eden"
+# print(name)
 
-name = input("What is your name?")
-length = len(name)
-print(length)
+# name = input("What is your name?")
+# length = len(name)
+# print(length)
+
+print("Welcome to the Band Name Generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
