0e4de4cea3117fb8c5babf60c1af217ab2f9a8b2
lineJeong <line.jeong32@gmail.com>
2023-06-02 19:19:59 +0900

    section1 - 12. 파이썬 변수

--- section1/main.py
+++ section1/main.py
@@ -1,3 +1,13 @@
 # print("Hello" + " " + "Angela")
 
-print("Hello " + input("what is your name?") + "!")
+# print("Hello " + input("what is your name?") + "!")
+
+name = "Jack"
+print(name)
+
+name = "Eden"
+print(name)
+
+name = input("What is your name?")
+length = len(name)
+print(length)
