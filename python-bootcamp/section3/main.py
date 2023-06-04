177edd0f7251168a51f2b45ee2e02c8df1147ec2
lineJeong <line.jeong32@gmail.com>
2023-06-04 19:19:42 +0900

    section3 - 35. 논리 연산자

--- python-bootcamp/section3/main.py
+++ python-bootcamp/section3/main.py
@@ -11,12 +11,15 @@ bill = 0
 if height >= 120:
     print("You can ride the rollercoaster!")
     age = int(input("What is your age? "))
+
     if age < 12:
         bill = 5
         print(f"Child tickets are ${bill}.")
     elif age <= 18:
         bill = 7
         print(f"Youth tickets are ${bill}.")
+    elif age >= 45 and age <= 55:
+        print("Everything is going to be ok. Have a free ride on us.")
     else:
         bill = 12
         print(f"Adult tickets are ${bill}.")
