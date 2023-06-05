8652157b8e376e6e54dfa2e6ba3db4008e3e4b91
lineJeong <line.jeong32@gmail.com>
2023-06-05 22:32:38 +0900

    section4 - 44. IndexError 및 중첩 리스트 활용하기

--- python-bootcamp/section4/main2.py
+++ python-bootcamp/section4/main2.py
@@ -1,11 +1,13 @@
 state1 = "Delaware"
 state2 = "Pennsylvania"
 
-states_of_america = ["Delaware", "Pennsylvania"]
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
 
-states_of_america[0] = "Changed"
+# states_of_america[0] = "Changed"
 
-states_of_america.append("append")
-states_of_america.extend(["extend1", "extend2"])
+# states_of_america.append("append")
+# states_of_america.extend(["extend1", "extend2"])
 
-print(states_of_america)
+num_of_state = len(states_of_america)
+
+print(states_of_america[num_of_state - 1])
---
+++ python-bootcamp/section4/main3.py
@@ -0,0 +1,7 @@
+# dirty_dozen = ["Strawberries", "Spianch", "Kale", "Nectarines", "Apples"]
+fruit = ["Strawberries", "Nectarines", "Apples"]
+vegetables = ["Spinach", "Kale"]
+
+dirty_dozen = [fruit, vegetables]
+
+print(dirty_dozen)
