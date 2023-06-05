e636f74f067b27a0e7b93652cb0ad2cde71bfe2f
lineJeong <line.jeong32@gmail.com>
2023-06-05 22:21:06 +0900

    section4 - 42. 오프셋을 이해하고 리스트에 항목 추가하기

---
+++ python-bootcamp/section4/main2.py
@@ -0,0 +1,11 @@
+state1 = "Delaware"
+state2 = "Pennsylvania"
+
+states_of_america = ["Delaware", "Pennsylvania"]
+
+states_of_america[0] = "Changed"
+
+states_of_america.append("append")
+states_of_america.extend(["extend1", "extend2"])
+
+print(states_of_america)
