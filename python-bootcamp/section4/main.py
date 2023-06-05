a72ff0938f108d5473b95c6a9b5db14dceca5364
lineJeong <line.jeong32@gmail.com>
2023-06-05 11:45:28 +0900

    section4 - 40. Random 모듈

---
+++ python-bootcamp/section4/main.py
@@ -0,0 +1,13 @@
+import random
+import my_module
+
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+print(my_module.pi)
+
+random_float = random.random()
+# print(random_float)
+
+random_0_to_5 = random_float * 5
+print(random_0_to_5)
