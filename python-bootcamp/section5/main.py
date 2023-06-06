db1011a79facf2e500fa208b876da36246ef3b18
lineJeong <line.jeong32@gmail.com>
2023-06-06 22:06:55 +0900

    section5 - 52. for 반복문과 range() 함수

--- python-bootcamp/section5/main.py
+++ python-bootcamp/section5/main.py
@@ -3,3 +3,12 @@ for fruit in fruits:
     print(fruit)
     print(fruit + " Pie")
 print(fruits)
+
+# For Loop with Range
+for number in range(1, 11, 3):
+    print(number)
+
+sum = 0
+for number in range(1, 101):
+    sum += number
+print(sum)
