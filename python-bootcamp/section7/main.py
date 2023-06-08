a62ef59de34c1c550044a378ec6b9c7971403be7
lineJeong <line.jeong32@gmail.com>
2023-06-08 21:55:19 +0900

    section7 - 75. 도전과제 4 해설 - 플레이어의 남은 목숨을 세는 법

--- python-bootcamp/section7/main.py
+++ python-bootcamp/section7/main.py
@@ -10,6 +10,63 @@
 
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
@@ -26,9 +83,10 @@ for _ in range(word_length):
     display_list += "_"
 
 print(chosen_word)
-print(display_list)
+print(f"{' '.join(display_list)}")
 
 end_of_game = False
+lives = 6
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
@@ -38,8 +96,16 @@ while not end_of_game:
         if letter == guess:
             display_list[position] = letter
 
+    if guess not in chosen_word:
+        lives -= 1
+
     print(display_list)
+    print(stages[lives])
 
     if "_" not in display_list:
         end_of_game = True
-        print("You win")
+        print("You win.")
+    
+    if lives == 0:
+        end_of_game = True
+        print("You lose.")
