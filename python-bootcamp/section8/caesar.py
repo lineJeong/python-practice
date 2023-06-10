89fe6e244e12f2e07ba0b3ccb819a4907b9aa07a
lineJeong <line.jeong32@gmail.com>
2023-06-10 16:50:02 +0900

    section8 - 87. 카이사르 암호 4부 - 유저 경험 개선 & 마무리

--- python-bootcamp/section8/caesar.py
+++ python-bootcamp/section8/caesar.py
@@ -1,30 +1,45 @@
+from caesar_art import logo
+
 alphabet = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ]
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+print(logo)
 
 def caesar(cipher_direction, start_text, shift_amount):
     end_text = ""
+    length_of_alphabet = len(alphabet)
+    shift_amount %= length_of_alphabet # shift 값이 26(alphabet 길이)보다 큰 경우 커버
 
     if cipher_direction == "decode":
         shift_amount *= -1
 
     for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        length_of_alphabet = len(alphabet)
-        if new_position >= length_of_alphabet: # encode에서만 해당 조건 발생
-            new_position -= length_of_alphabet
-
-        end_text += alphabet[new_position]
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            new_position = position + shift_amount
+            if new_position >= length_of_alphabet: # encode에서만 해당 조건 발생
+                new_position -= length_of_alphabet
+            end_text += alphabet[new_position]
+        else:
+            end_text += letter # alphabet 외의 문자는 그대로 출력
 
     print(f"The {cipher_direction}d text is {end_text}")
 
-caesar(cipher_direction=direction, start_text=text, shift_amount=shift)
+# 재실행 로직 추가
+should_continue = True
+
+while should_continue == True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(cipher_direction=direction, start_text=text, shift_amount=shift)
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if result == 'no':
+        should_continue = False
+        print("Goodbye")
 
 # def caesar(cipher_direction, start_text, shift_amount):
 #     end_text = ""
