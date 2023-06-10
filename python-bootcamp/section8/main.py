d7363009569e135f906f9025610e803238985011
lineJeong <line.jeong32@gmail.com>
2023-06-10 12:25:47 +0900

    section8 - 81. 위치 인자와 키워드 인자

--- python-bootcamp/section8/main.py
+++ python-bootcamp/section8/main.py
@@ -1,6 +1,15 @@
-def greet_with_name(name):
-    print(f"Hello, {name}")
-    print("How do you do?")
-    print("Isn't the weather nice today?")
+# def greet_with_name(name):
+#     print(f"Hello, {name}")
+#     print("How do you do?")
+#     print("Isn't the weather nice today?")
 
-greet_with_name("line.jeong32")
+# greet_with_name("line.jeong32")
+
+def greet_with(name, location):
+    print(f"hello {name}")
+    print(f"what is it like in {location}")
+
+greet_with("jeong", "Texas")
+
+# Keyword Arguments
+greet_with(location="Texas", name="jeong")
