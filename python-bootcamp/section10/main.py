d08c8c2ce4c3122817c73fb062c31369c9ce413a
lineJeong <line.jeong32@gmail.com>
2023-06-11 11:29:29 +0900

    section10 - 99. 다양한 리턴값

--- python-bootcamp/section10/main.py
+++ python-bootcamp/section10/main.py
@@ -1,9 +1,15 @@
 # Function with Outputs
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+
     full_name = f"{f_name} {l_name}"
     formated_full_name = full_name.title()
     return formated_full_name
 
 
-formated_name = format_name("first", "LAST")
-print(f"hello, {formated_name}")
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+
+formated_name = format_name(first_name, last_name)
+print(f"{formated_name}")
