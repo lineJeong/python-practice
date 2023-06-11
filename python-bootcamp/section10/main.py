3c695bcdf6d0e7a555cc0b52667e0c1dca5b41a1
lineJeong <line.jeong32@gmail.com>
2023-06-11 12:14:45 +0900

    section10 - 101. 독스트링

--- python-bootcamp/section10/main.py
+++ python-bootcamp/section10/main.py
@@ -1,5 +1,8 @@
 # Function with Outputs
 def format_name(f_name, l_name):
+    # docstring
+    """Take a first and last name and format it 
+    to return the title case version of the name."""
     if f_name == "" or l_name == "":
         return "You didn't provide valid inputs."
 
