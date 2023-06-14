1b1400e0b942d703a2a083f2823010eb030dbfe2
lineJeong <line.jeong32@gmail.com>
2023-06-14 20:49:17 +0900

    section13 - 125. 컴퓨터의 입장에서 생각해보기

--- python-bootcamp/section13/main.py
+++ python-bootcamp/section13/main.py
@@ -35,3 +35,19 @@ my_function()
 dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
 dice_num = randint(0, 5)
 print(dice_imgs[dice_num])
+
+
+# # 3. Play Computer
+# year = int(input("What's your year of birth? "))
+# if year > 1980 and year < 1994:
+#     print("You are a millenial.")
+# elif year > 1994:
+#     print("You are a Gen Z.")
+
+
+# # Fix
+year = int(input("What's your year of birth? "))
+if year > 1980 and year < 1994:
+    print("You are a millenial.")
+elif year >= 1994:
+    print("You are a Gen Z.")
