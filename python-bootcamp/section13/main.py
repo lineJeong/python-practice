1d20df435d993630670fa86f2cbb922ddeb2f942
lineJeong <line.jeong32@gmail.com>
2023-06-14 20:41:34 +0900

    section13 - 124. 버그 재현하기

--- python-bootcamp/section13/main.py
+++ python-bootcamp/section13/main.py
@@ -1,16 +1,21 @@
 ############ DEBUGGING############
+from random import randint
 
-# # Describe Problem
-def my_function():
-    for i in range(1, 20):  # 1~19
-        if i == 20:
-            print("You got it")
+# # 1. Describe Problem
 
 
-my_function()
+# def my_function():
+#     for i in range(1, 20):  # 1~19
+#         if i == 20:
+#             print("You got it")
+
+
+# my_function()
 
 
 # # Fix
+
+
 def my_function():
     for i in range(1, 21):
         if i == 20:
@@ -18,3 +23,15 @@ def my_function():
 
 
 my_function()
+
+
+# # 2. Reproduce the Bug
+# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(1, 6)
+# print(dice_imgs[dice_num])
+
+
+# # Fix
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(0, 5)
+print(dice_imgs[dice_num])
