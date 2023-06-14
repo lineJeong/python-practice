4c234fe348db17506ffd6a9bf00b8ec8e7b638d2
lineJeong <line.jeong32@gmail.com>
2023-06-14 15:25:58 +0900

    section12 - 117. 파이썬도 블록 유효 범위가 있나요?

--- python-bootcamp/section12/main.js
+++ python-bootcamp/section12/main.js
@@ -7,3 +7,16 @@ function increase_enemies() {
 
 increase_enemies();
 console.log(`enemies out function: ${enemies}`); // 2
+
+if (3 > 2) {
+  const aVariable = 10;
+  console.log(`a_variable : ${aVariable}`);
+}
+
+game_level = 3;
+enemies = ["Skeleton", "Zombie", "Alien"];
+if (game_level < 5) {
+  var new_enemy = enemies[0]; // var면 블록스코프 X, let, const면 블록스코프 O
+}
+
+console.log(new_enemy);
--- python-bootcamp/section12/main.py
+++ python-bootcamp/section12/main.py
@@ -34,3 +34,18 @@ def game():
 
 game()
 print(player_health)
+
+# There is no Block Scope
+# 둘러싼 함수와 같은 스코프를 가짐, 둘러싼 함수가 없으면 전역 스코프를 가짐
+
+if 3 > 2:
+    a_variable = 10
+    print(f"a_variable : {a_variable}")
+
+
+game_level = 3
+enemies = ["Skeleton", "Zombie", "Alien"]
+if game_level < 5:
+    new_enemy = enemies[0]
+
+print(new_enemy)
