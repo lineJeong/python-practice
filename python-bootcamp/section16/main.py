819620ce30f0a469b2996dca638d20433357f1ac
lineJeong <line.jeong32@gmail.com>
2023-06-23 22:18:50 +0900

    section16 - 149. 객체 속성 변경 및 메소드 호출 연습

--- python-bootcamp/section16/main.py
+++ python-bootcamp/section16/main.py
@@ -1,14 +1,25 @@
-# import another_module
-# print(another_module.another_variable)
+# # import another_module
+# # print(another_module.another_variable)
 
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(100)
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("Poketmon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+table.align = "l"
+
+print(table)
