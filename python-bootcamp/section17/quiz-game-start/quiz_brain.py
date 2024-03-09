148db439d0cc2e7e5f974bb33ecf25ea3d7523ee
lineJeong <line.jeong32@gmail.com>
2024-03-09 14:39:50 -0600

    section17 - 160. 퀴즈 프로젝트 3부: 퀴즈브레인과 next_question() 메소드

---
+++ python-bootcamp/section17/quiz-game-start/quiz_brain.py
@@ -0,0 +1,15 @@
+#TODO: asking the questions
+#TODO: checking if the answer was correct
+#TODO: checking if we're the end of the quiz
+
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.questions_list = q_list
+
+    def next_question(self):
+        current_question = self.questions_list[self.question_number]
+        self.question_number += 1
+        input(f"Q.{self.question_number}: {current_question.text} (True/False)? ")
+
+
