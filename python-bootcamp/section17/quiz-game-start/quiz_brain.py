77cbc7096e4999a94cbcee64571efe63ef006423
lineJeong <line.jeong32@gmail.com>
2024-03-09 15:03:43 -0600

    section17 - 161. 퀴즈 프로젝트 4부: 새로운 질문을 계속 보여주는 방법

--- python-bootcamp/section17/quiz-game-start/quiz_brain.py
+++ python-bootcamp/section17/quiz-game-start/quiz_brain.py
@@ -7,9 +7,10 @@ class QuizBrain:
         self.question_number = 0
         self.questions_list = q_list
 
+    def still_has_questions(self):
+        return self.question_number < len(self.questions_list)
+
     def next_question(self):
         current_question = self.questions_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question.text} (True/False)? ")
-
-
+        input(f"Q.{self.question_number}: {current_question.text} (True/False)?: ")
