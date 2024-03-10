6bc1600a1fc2b3807e60680a5b5ebd23a693fd4d
lineJeong <line.jeong32@gmail.com>
2024-03-10 12:58:18 -0500

    section18 - 166. 터틀 그래픽 이해하기 및 문서 사용법

---
+++ python-bootcamp/section18/main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("SeaGreen")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+
+screen = Screen()
+screen.exitonclick()
