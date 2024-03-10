586aa490498780474763d9bde89b2b755ecdb6ad
lineJeong <line.jeong32@gmail.com>
2024-03-10 15:48:28 -0500

    section18 - 169. 터틀 과제 2 - 점선 그리기

--- python-bootcamp/section18/main.py
+++ python-bootcamp/section18/main.py
@@ -4,9 +4,11 @@ tim = Turtle()
 tim.shape("turtle")
 tim.color("SeaGreen")
 
-for _ in range(4):
-    tim.forward(100)
-    tim.right(90)
+for _ in range(20):
+    tim.forward(5)
+    tim.penup()
+    tim.forward(5)
+    tim.pendown()
 
 screen = Screen()
 screen.exitonclick()
