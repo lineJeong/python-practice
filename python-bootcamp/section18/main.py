915d2f26fa7430402acdea79509c0777e7c453f5
lineJeong <line.jeong32@gmail.com>
2024-03-14 23:09:18 -0500

    section19 - 170. 터틀 과제 3 - 다양한 도형 그리기

--- python-bootcamp/section18/main.py
+++ python-bootcamp/section18/main.py
@@ -1,14 +1,38 @@
 from turtle import Turtle, Screen
+import random
 
 tim = Turtle()
 tim.shape("turtle")
 tim.color("SeaGreen")
 
-for _ in range(20):
-    tim.forward(5)
-    tim.penup()
-    tim.forward(5)
-    tim.pendown()
+# for n in range(3, 11):
+#     angle = 360 / n
+#     for _ in range(n):
+#         tim.forward(100)
+#         tim.right(angle)
+
+colors = [
+    "aquamarine",
+    "medium aquamarine",
+    "dark sea green",
+    "medium sea green",
+    "sea green",
+    "green yellow",
+    "lime",
+    "olive drab",
+]
+
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colors))
+    draw_shape(shape_side_n)
 
 screen = Screen()
 screen.exitonclick()
