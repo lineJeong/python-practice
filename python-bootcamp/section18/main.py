1afd33e9b15f7a2c4db78c96b028138aa46075fe
lineJeong <line.jeong32@gmail.com>
2024-03-16 20:47:20 -0500

    section19 - 171. 터틀 과제 4 - 무작위 행보 구현하기

--- python-bootcamp/section18/main.py
+++ python-bootcamp/section18/main.py
@@ -5,34 +5,42 @@ tim = Turtle()
 tim.shape("turtle")
 tim.color("SeaGreen")
 
+colors = [
+    "aquamarine",
+    "IndianRed",
+    "dark sea green",
+    "medium sea green",
+    "DarkOrchid",
+    "green yellow",
+    "lime",
+    "olive drab",
+]
+
+directions = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
+
+for _ in range(200):
+    tim.color(random.choice(colors))
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
+
 # for n in range(3, 11):
 #     angle = 360 / n
 #     for _ in range(n):
 #         tim.forward(100)
 #         tim.right(angle)
 
-colors = [
-    "aquamarine",
-    "medium aquamarine",
-    "dark sea green",
-    "medium sea green",
-    "sea green",
-    "green yellow",
-    "lime",
-    "olive drab",
-]
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for _ in range(num_sides):
+#         tim.forward(100)
+#         tim.right(angle)
 
 
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
-
-
-for shape_side_n in range(3, 11):
-    tim.color(random.choice(colors))
-    draw_shape(shape_side_n)
+# for shape_side_n in range(3, 11):
+#     tim.color(random.choice(colors))
+#     draw_shape(shape_side_n)
 
 screen = Screen()
 screen.exitonclick()
